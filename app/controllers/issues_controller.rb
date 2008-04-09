alfajango/redmine

commit 60fa56c641d2346af81ac07fed3776504274d493

Fixed: trying to preview a new issue raises an exception with postgresql (close #984).
git-svn-id: http://redmine.rubyforge.org/svn/trunk@1322 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -344,8 +344,8 @@ class IssuesController < ApplicationController
   end
   
   def preview
-    issue = @project.issues.find_by_id(params[:id])
-    @attachements = issue.attachments if issue
+    @issue = @project.issues.find_by_id(params[:id]) if params[:id]
+    @attachements = issue.attachments if @issue
     @text = params[:notes] || (params[:issue] ? params[:issue][:description] : nil)
     render :partial => 'common/preview'
   end

app/views/issues/new.rhtml
@@ -8,7 +8,7 @@
     </div>
     <%= submit_tag l(:button_create) %>
     <%= link_to_remote l(:label_preview), 
-                       { :url => { :controller => 'issues', :action => 'preview', :project_id => @project, :id => @issue },
+                       { :url => { :controller => 'issues', :action => 'preview', :project_id => @project },
                          :method => 'post',
                          :update => 'preview',
                          :with => "Form.serialize('issue-form')",

commit 0210dca8da5130fa1705632ae202212503ef52c7

Fixed: preview fails when updating an issue (broken by r1322). Patch #1027.

app/controllers/issues_controller.rb
@@ -345,7 +345,7 @@ class IssuesController < ApplicationController
   
   def preview
     @issue = @project.issues.find_by_id(params[:id]) if params[:id]
-    @attachements = issue.attachments if @issue
+    @attachements = @issue.attachments if @issue
     @text = params[:notes] || (params[:issue] ? params[:issue][:description] : nil)
     render :partial => 'common/preview'
   end