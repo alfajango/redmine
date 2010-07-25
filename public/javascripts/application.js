alfajango/redmine

commit 49226dc68a1c86d661fd92a145f43a361abb0e63

Convert the file attachment's description to a label.
git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3750 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/views/attachments/_form.rhtml
@@ -1,7 +1,7 @@
 <span id="attachments_fields">
 <%= file_field_tag 'attachments[1][file]', :size => 30, :id => nil  -%>
-<%= text_field_tag 'attachments[1][description]', '', :size => 60, :id => nil %>
-<em><%= l(:label_optional_description) %></em>
+<label class="inline"><span id="attachment_description_label_content"><%= l(:label_optional_description) %></span><%= text_field_tag 'attachments[1][description]', '', :size => 60, :id => nil %>
+</label>
 </span>
 <br />
 <small><%= link_to l(:label_add_another_file), '#', :onclick => 'addFileField(); return false;' %>

public/javascripts/application.js
@@ -52,11 +52,17 @@ function addFileField() {
     d.type = "text";
     d.name = "attachments[" + fileFieldCount + "][description]";
     d.size = 60;
+    var dLabel = document.createElement("label");
+    dLabel.addClassName('inline');
+    // Pulls the languge value used for Optional Description
+    dLabel.update($('attachment_description_label_content').innerHTML)
     
     p = document.getElementById("attachments_fields");
     p.appendChild(document.createElement("br"));
     p.appendChild(f);
-    p.appendChild(d);
+    p.appendChild(dLabel);
+    dLabel.appendChild(d);
+
 }
 
 function showTab(name) {

app/views/issues/_form.rhtml
@@ -27,7 +27,7 @@
 </div>
 
 <% if @issue.new_record? %>
-<p><label><%=l(:label_attachment_plural)%></label><%= render :partial => 'attachments/form' %></p>
+<p><%= label_tag('attachments[1][file]', l(:label_attachment_plural))%><%= render :partial => 'attachments/form' %></p>
 <% end %>
 
 <% if @issue.new_record? && User.current.allowed_to?(:add_issue_watchers, @project) -%>

public/stylesheets/application.css
@@ -404,6 +404,12 @@ display: block;
 width: auto;
 }
 
+.tabular label.inline{
+float:none;
+margin-left: 5px !important;
+width: auto;
+}
+
 input#time_entry_comments { width: 90%;}
 
 #preview fieldset {margin-top: 1em; background: url(../images/draft.png)}

commit 13868e6e62c08b7bd94ddf7a504b2750181f0e76

Fixed: Add Another file to ticket doesn't work in IE (broken by r3750).

app/views/attachments/_form.rhtml
@@ -1,6 +1,5 @@
 <span id="attachments_fields">
-<%= file_field_tag 'attachments[1][file]', :size => 30, :id => nil  -%>
-<label class="inline"><span id="attachment_description_label_content"><%= l(:label_optional_description) %></span><%= text_field_tag 'attachments[1][description]', '', :size => 60, :id => nil %>
+<%= file_field_tag 'attachments[1][file]', :size => 30, :id => nil  -%><label class="inline"><span id="attachment_description_label_content"><%= l(:label_optional_description) %></span><%= text_field_tag 'attachments[1][description]', '', :size => 60, :id => nil %>
 </label>
 </span>
 <br />

public/javascripts/application.js
@@ -52,7 +52,7 @@ function addFileField() {
     d.type = "text";
     d.name = "attachments[" + fileFieldCount + "][description]";
     d.size = 60;
-    var dLabel = document.createElement("label");
+    var dLabel = new Element('label');
     dLabel.addClassName('inline');
     // Pulls the languge value used for Optional Description
     dLabel.update($('attachment_description_label_content').innerHTML)