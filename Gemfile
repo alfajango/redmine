f0a28dc89798ae95b180d4d9c9b2fd771370c55e
Steve Schwartz <steve@alfajango.com>
2021-09-15 01:52:22 -0400

    Removed rails_12factor gem no longer needed for rails 5 apps on heroku.

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'globalid', '~> 0.4.2' if Gem.ruby_version < Gem::Version.new('2.6.0')
 gem 'rouge', '~> 3.26.0'
 gem 'request_store', '~> 1.5.0'
 gem "mini_mime", "~> 1.0.1"
-gem "rails_12factor"
 gem "actionpack-xml_parser"
 gem 'roadie-rails', (RUBY_VERSION < '2.5' ? '~> 1.3.0' : '~> 2.2.0')
 gem 'marcel'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,4 +31,13 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   config.force_ssl = true
+
+  # Replacing rails_12factor gem for Heroku
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
